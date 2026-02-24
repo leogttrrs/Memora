0b6cd946a09a2bd02bfd29da7357ab59b62dad30
Leonardo Gutterres <leogttrrs@gmail.com>
2026-02-24 20:49:50 -0300

    Mudando placeholders

--- memora_server/static/js/script.js
+++ memora_server/static/js/script.js
@@ -10,13 +10,11 @@ function atualizarNomeArquivo(input) {
     const textoElemento = document.getElementById('texto-arquivo');
 
     if (input.files && input.files.length > 0) {
-        // Pega o nome do arquivo (ex: "batman.jpg")
         textoElemento.textContent = input.files[0].name;
-        textoElemento.style.color = "#cbd5e1"; // Muda cor para branco/claro
+        textoElemento.style.color = "#cbd5e1";
     } else {
-        // Se cancelou
         textoElemento.textContent = "Nenhum arquivo escolhido";
-        textoElemento.style.color = "#94a3b8"; // Volta para cinza
+        textoElemento.style.color = "#94a3b8";
     }
 }
 
@@ -34,11 +32,9 @@ function atualizarInput(input) {
     const texto = document.getElementById('file-text');
 
     if (input.files && input.files.length > 0) {
-        // Muda o texto e ativa o Slide
         texto.textContent = input.files[0].name;
-        box.classList.add('has-file'); // <--- ISSO DISPARA A ANIMAÇÃO CSS
+        box.classList.add('has-file');
     } else {
-        // Reseta
         texto.textContent = "Selecione uma imagem...";
         box.classList.remove('has-file');
     }
@@ -62,19 +58,15 @@ document.getElementById('modal-nota-overlay').addEventListener('click', function
     }
 });
 
-// Função para abrir o Lightbox
 function abrirLightbox(caminhoImagem) {
     const lightbox = document.getElementById('lightbox');
     const img = document.getElementById('lightbox-img');
 
-    // Define a imagem que foi clicada
     img.src = caminhoImagem;
 
-    // Mostra o overlay
     lightbox.style.display = 'flex';
 }
 
-// Função para fechar (clicando no fundo ou no X)
 function fecharLightbox() {
     document.getElementById('lightbox').style.display = 'none';
 }
@@ -82,19 +74,16 @@ function fecharLightbox() {
 function adicionarCampoCidade() {
     const container = document.getElementById('cities-container');
 
-    // Cria a div que envolve (wrapper)
     const div = document.createElement('div');
     div.className = 'city-input-group';
 
-    // Cria o input novo
     const input = document.createElement('input');
     input.type = 'text';
-    input.name = 'cidades'; // O segredo está aqui: mesmo nome!
+    input.name = 'cidades';
     input.placeholder = 'Nome da cidade';
     input.required = true;
     input.autocomplete = 'off';
 
-    // Cria o botão de remover (X)
     const btnRemove = document.createElement('button');
     btnRemove.type = 'button';
     btnRemove.className = 'btn-remove-city';
@@ -103,7 +92,6 @@ function adicionarCampoCidade() {
         container.removeChild(div);
     };
 
-    // Monta tudo
     div.appendChild(input);
     div.appendChild(btnRemove);
     container.appendChild(div);
